e4c94fe474e648c8c9b12233c91c68d85ba6e0a5
boomyao <zhangyaoruo@outlook.com>
2016-12-31 16:46:58 +0800

    edit something

--- glory-carousel.js
+++ glory-carousel.js
@@ -10,15 +10,15 @@ function GloryCarousel(options) {
         throw "require more than 4 pictures";
     }
 
-    var root,gloryInner;
+    var root, gloryInner;
     var activeElmNo, items, canAnimate = true;
-    var PZ = options.sizeRate||9 / 16, PU = options.expendRate|| 7 / 8;//图片比例，展开和常规比例
+    var PZ = options.sizeRate || 9 / 16, PU = options.expendRate || 7 / 8;//图片比例，展开和常规比例
     var gloryWidth, gloryHeight, ITEM_W, ITEM_H, EX_W, EX_H, INNER_DEFAULT_LEFT;
 
     ///public methods
     //计算尺寸
     this.initSize = function (width, height) {
-        calcBaseSize(width,height);
+        calcBaseSize(width, height);
         root.style.width = gloryWidth + 'px';
         root.style.height = gloryHeight + 'px';
         gloryInner.style.height = gloryHeight + 'px';
@@ -30,48 +30,70 @@ function GloryCarousel(options) {
         items[activeElmNo].style.left = INNER_DEFAULT_LEFT + 'px';
         expand(items[activeElmNo]);
     }
-    //向右滚动
+    ////向右滚动
+    // this.next = function () {
+    //     if (canAnimate) {
+    //         canAnimate = false;
+    //         var hadDiff = 0;
+    //         var perVal = 15;
+    //         narrow(items[activeElmNo]);
+    //         if (activeElmNo < items.length - 1) { activeElmNo++; }
+    //         else { activeElmNo = 0; }
+    //         expand(items[activeElmNo]);
+    //         var timer = setInterval(function () {
+    //             gloryInner.style.left = gloryInner.offsetLeft - perVal + 'px';
+    //             hadDiff += perVal;
+    //             if (hadDiff >= ITEM_W) {
+    //                 clearInterval(timer);
+    //                 calcItemLeft(activeElmNo);
+    //                 gloryInner.style.left = 0;
+    //                 canAnimate = true;
+    //             }
+    //         }, 20)
+    //     }
+    // }
+    // //向左滚动
+    // this.previous = function () {
+    //     if (canAnimate) {
+    //         canAnimate = false;
+    //         var hadDiff = 0;
+    //         var perVal = 15;
+    //         narrow(items[activeElmNo]);
+    //         if (activeElmNo > 0) { activeElmNo--; }
+    //         else { activeElmNo = items.length - 1; }
+    //         expand(items[activeElmNo]);
+    //         var timer = setInterval(function () {
+    //             gloryInner.style.left = gloryInner.offsetLeft + perVal + 'px';
+    //             hadDiff += perVal;
+    //             if (hadDiff >= ITEM_W) {
+    //                 clearInterval(timer);
+    //                 calcItemLeft(activeElmNo);
+    //                 gloryInner.style.left = 0;
+    //                 canAnimate = true;
+    //             }
+    //         }, 20)
+    //     }
+    // }
+
     this.next = function () {
         if (canAnimate) {
             canAnimate = false;
-            var hadDiff = 0;
-            var perVal = 15;
-            narrow(items[activeElmNo]);
+            narrow(items[activeElmNo])
+            moveToLeft();
             if (activeElmNo < items.length - 1) { activeElmNo++; }
             else { activeElmNo = 0; }
-            expand(items[activeElmNo]);
-            var timer = setInterval(function () {
-                gloryInner.style.left = gloryInner.offsetLeft - perVal + 'px';
-                hadDiff += perVal;
-                if (hadDiff >= ITEM_W) {
-                    clearInterval(timer);
-                    calcItemLeft(activeElmNo);
-                    gloryInner.style.left = 0;
-                    canAnimate = true;
-                }
-            }, 20)
+            expand(items[activeElmNo])
         }
     }
-    //向左滚动
-    this.previous = function () {
+
+    this.previous=function(){
         if (canAnimate) {
             canAnimate = false;
-            var hadDiff = 0;
-            var perVal = 15;
-            narrow(items[activeElmNo]);
+            narrow(items[activeElmNo])
+            moveToRight();
             if (activeElmNo > 0) { activeElmNo--; }
             else { activeElmNo = items.length - 1; }
-            expand(items[activeElmNo]);
-            var timer = setInterval(function () {
-                gloryInner.style.left = gloryInner.offsetLeft + perVal + 'px';
-                hadDiff += perVal;
-                if (hadDiff >= ITEM_W) {
-                    clearInterval(timer);
-                    calcItemLeft(activeElmNo);
-                    gloryInner.style.left = 0;
-                    canAnimate = true;
-                }
-            }, 20)
+            expand(items[activeElmNo])
         }
     }
 
@@ -85,7 +107,7 @@ function GloryCarousel(options) {
         gloryInner = document.createElement("div");
         gloryInner.className = "glory-inner";
         root.appendChild(gloryInner);
-        calcBaseSize(options.width,options.height);
+        calcBaseSize(options.width, options.height);
         activeElmNo = 0;
         for (var i = 0; i < imgPaths.length; i++) {
             var gloryItem = document.createElement("div");
@@ -99,12 +121,48 @@ function GloryCarousel(options) {
             img.src = imgPaths[i];
             items[i].appendChild(img);
         }
-        
+
+    }
+
+    function moveToLeft() {
+        var hadDiff = 0;
+        var perVal = 30;
+        var timer = setInterval(function () {
+            if (ITEM_W - hadDiff < perVal) {
+                gloryInner.style.left = gloryInner.offsetLeft - (ITEM_W - hadDiff) + 'px';
+            } else {
+                gloryInner.style.left = gloryInner.offsetLeft - perVal + 'px';
+            }
+            hadDiff += perVal;
+            if (hadDiff >= ITEM_W) {
+                clearInterval(timer);
+                calcItemLeft(activeElmNo);
+                gloryInner.style.left = 0;
+            }
+        }, 15)
+    }
+
+    function moveToRight() {
+        var hadDiff = 0;
+        var perVal = 30;
+        var timer = setInterval(function () {
+            if (ITEM_W - hadDiff < perVal) {
+                gloryInner.style.left = gloryInner.offsetLeft + (ITEM_W - hadDiff) + 'px';
+            } else {
+                gloryInner.style.left = gloryInner.offsetLeft + perVal + 'px';
+            }
+            hadDiff += perVal;
+            if (hadDiff >= ITEM_W) {
+                clearInterval(timer);
+                calcItemLeft(activeElmNo);
+                gloryInner.style.left = 0;
+            }
+        }, 15)
     }
 
     //缩小动作
-    function narrow(elm) {
-        var perW = 4;
+    function narrow(elm, callback) {
+        var perW = 6;
         var perH = perW * PZ;
         var exW = (1 - PU) * ITEM_W;
         var hadDiff = 0;
@@ -117,12 +175,12 @@ function GloryCarousel(options) {
                 clearInterval(timer);
                 elm.style.zIndex = 0;
             }
-        }, 30)
+        }, 5)
     }
     //放大动作
     function expand(elm) {
         elm.style.zIndex = 10;
-        var perW = 4;
+        var perW = 6;
         var perH = perW * PZ;
         var exW = (1 - PU) * ITEM_W;
         //var diff = 30;
@@ -134,8 +192,9 @@ function GloryCarousel(options) {
             elm.style.left = elm.offsetLeft - 0.5 * perW + 'px';
             if (hadDiff >= exW) {
                 clearInterval(timer);
+                canAnimate = true;
             }
-        }, 30)
+        }, 5)
     }
     //计算item位置
     function calcItemLeft(n) {
@@ -170,7 +229,7 @@ function GloryCarousel(options) {
         }
     }
     //以组件长宽计算基础尺寸数值
-    function calcBaseSize(width,height) {
+    function calcBaseSize(width, height) {
         gloryWidth = width || 400;
         gloryHeight = height || 100;
         ITEM_W = gloryHeight * PU / PZ;
